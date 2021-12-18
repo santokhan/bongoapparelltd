31c1170d96d7a901fded5db354341611a614fd56
Santo <santokhan1999@gmail.com>
2021-12-18 15:19:13 +0600

    Add images

---
+++ theme/navbar.php
@@ -0,0 +1,35 @@
+<div class="container-fluid px-0">
+    <div class="nav-container bg-nav px-3">
+        <div class="nav_bar">
+            <button type="button" class="toggler" id="navToggler" aria-label="Nav Toggler">
+                <img src="assets/svg/toggler.svg" alt="Toggler">
+            </button>
+
+            <nav class="navs" id="navBar">
+                <div class="item"><a class="link" href="product.php?category=all">All</a></div>
+                <div class="item"><a class="link" href="product.php?category=deals">Today's Deals</a></div>
+                <div class="item"><a class="link" href="product.php?category=customer">Customer Service</a></div>
+                <div class="item"><a class="link" href="product.php?category=registry">Registry</a></div>
+                <div class="item"><a class="link" href="product.php?category=gift">Gift Cards</a></div>
+                <div class="item"><a class="link" href="product.php?category=sell">Sell</a></div>
+            </nav>
+        </div>
+
+        <div class="social">
+            <div class="media">
+                <a class="icon" target="_blank" rel="noreferrer" aria-label="Facebook" href="https://www.facebook.com/shahan.shah.520">
+                    <img src="assets/social_icon/facebook.svg" alt="Facebook">
+                </a>
+                <a class="icon" target="_blank" rel="noreferrer" aria-label="Twitter" href="https://twitter.com/shahan.shah.520">
+                    <img src="assets/social_icon/twitter.svg" alt="Twitter">
+                </a>
+                <a class="icon" target="_blank" rel="noreferrer" aria-label="Linkedin" href="https://www.linkedin.com/company/76264642/admin/">
+                    <img src="assets/social_icon/linkedin.svg" alt="Linkedin">
+                </a>
+                <a class="icon" target="_blank" rel="noreferrer" aria-label="Instagram" href="https://www.instagram.com/santokhan1999/?hl=en">
+                    <img src="assets/social_icon/instagram.svg" alt="Instagram">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
